fd02c94197a8a0d62ea874928e00dfaf6f466589
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2024-08-11 22:14:40 +0300

    Mock out tests etc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,74 @@
-export const one = 1
-export const two = 2
+import { useCallback, useEffect, type Reducer, useReducer } from "react";
+import { useLocalStorage } from "usehooks-ts";
+
+type SyncAction<S> = { type: "sync"; payload: S };
+type Reducer<S, A> = (state: S, action: A | SyncAction<S>) => S;
+export type ReducerMiddlewareFn<S, A> = (
+  action: A | SyncAction<S>,
+  state?: S,
+) => void;
+
+export const useLocalStorageReducer = <S, A extends Object>(
+  key: string,
+  reducer: Reducer<S, A>,
+  initialState: S | (() => S),
+  middlewareFns: Array<ReducerMiddlewareFn<S, A>> = [],
+  afterwareFns: Array<ReducerMiddlewareFn<S, A>> = [],
+  options?: {
+    // TODO: may want to provide customer serializer/deserializer here
+    initializeWithValue?: boolean;
+  },
+) => {
+  const [savedState, setSavedState] = useLocalStorage<S>(
+    key,
+    initialState,
+    options,
+  );
+
+  const localStorageReducer = useCallback(
+    (state: S, action: A | SyncAction<S>) => {
+      if ("type" in action && action.type === "sync") {
+        return action.payload;
+      }
+      const newState = reducer(state, action as A);
+      setSavedState(newState);
+      return newState;
+    },
+    [reducer, setSavedState],
+  );
+
+  const [_, dispatch] = useReducerWithMiddleware(
+    localStorageReducer,
+    savedState,
+    middlewareFns,
+    afterwareFns,
+  );
+
+  // resync the reducer state with savedState
+  useEffect(() => {
+    dispatch({ type: "sync", payload: savedState });
+  }, [savedState, dispatch]);
+
+  return [savedState, dispatch] as const;
+};
+
+export const useReducerWithMiddleware = <S, A>(
+  // TODO: creae a type like Reducer for this
+  reducer: (state: S, action: A | SyncAction<S>) => S,
+  initialState: S,
+  middlewareFns: Array<ReducerMiddlewareFn<S, A>>,
+  afterwareFns: Array<ReducerMiddlewareFn<S, A>>,
+) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const dispatchWithMiddleware = useCallback(
+    (action: A | SyncAction<S>) => {
+      middlewareFns.forEach((middlewareFn) => middlewareFn(action, state));
+      dispatch(action);
+      afterwareFns.forEach((afterwareFn) => afterwareFn(action, state));
+    },
+    [middlewareFns, afterwareFns, state],
+  );
+
+  return [state, dispatchWithMiddleware] as const;
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
-import { describe, it, expect } from 'bun:test'
-import { one, two } from '../src'
+import { renderHook } from "@testing-library/react";
+import { describe, it, expect } from "bun:test";
+import { useLocalStorageReducer } from "../src/";
 
-describe('should', () => {
-  it('export 1', () => {
-    expect(one).toBe(1)
-  })
-
-  it('export 2', () => {
-    expect(two).toBe(2)
-  })
-})
+describe("should", () => {
+  it("example", () => {
+    const { result } = renderHook(() =>
+      useLocalStorageReducer("key", (state, action) => state, 0),
+    );
+    expect(result.current[0]).toBe(0);
+  });
+});
