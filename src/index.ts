177d258708e71ac34df9bab478125d52d1be14e2
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2024-08-11 22:47:39 +0300

    Run biome over repo

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,75 @@
-import { useCallback, useEffect, type Reducer, useReducer } from "react";
-import { useLocalStorage } from "usehooks-ts";
+import { type Reducer, useCallback, useEffect, useReducer } from "react"
+import { useLocalStorage } from "usehooks-ts"
 
-type SyncAction<S> = { type: "sync"; payload: S };
-type AugmentedReducer<S, A> = (state: S, action: A | SyncAction<S>) => S;
+const SYNC_ACTION_TYPE = "_sync"
+type SyncAction<S> = { type: typeof SYNC_ACTION_TYPE; payload: S }
+type AugmentedReducer<S, A> = (state: S, action: A | SyncAction<S>) => S
 export type ReducerMiddlewareFn<S, A> = (
-  action: A | SyncAction<S>,
-  state?: S,
-) => void;
+	action: A | SyncAction<S>,
+	state?: S,
+) => void
 
 export const useLocalStorageReducer = <S, A extends Object>(
-  key: string,
-  reducer: Reducer<S, A>,
-  initialState: S | (() => S),
-  middlewareFns: Array<ReducerMiddlewareFn<S, A>> = [],
-  afterwareFns: Array<ReducerMiddlewareFn<S, A>> = [],
-  options?: {
-    // TODO: may want to provide customer serializer/deserializer here
-    initializeWithValue?: boolean;
-  },
+	key: string,
+	reducer: Reducer<S, A>,
+	initialState: S | (() => S),
+	middlewareFns: Array<ReducerMiddlewareFn<S, A>> = [],
+	afterwareFns: Array<ReducerMiddlewareFn<S, A>> = [],
+	options?: {
+		// TODO: may want to provide customer serializer/deserializer here
+		initializeWithValue?: boolean
+	},
 ) => {
-  const [savedState, setSavedState] = useLocalStorage<S>(
-    key,
-    initialState,
-    options,
-  );
+	const [savedState, setSavedState] = useLocalStorage<S>(
+		key,
+		initialState,
+		options,
+	)
 
-  const localStorageReducer = useCallback(
-    (state: S, action: A | SyncAction<S>) => {
-      if ("type" in action && action.type === "sync") {
-        return action.payload;
-      }
-      const newState = reducer(state, action as A);
-      setSavedState(newState);
-      return newState;
-    },
-    [reducer, setSavedState],
-  );
+	const localStorageReducer = useCallback(
+		(state: S, action: A | SyncAction<S>) => {
+			if ("type" in action && action.type === SYNC_ACTION_TYPE) {
+				return action.payload
+			}
+			const newState = reducer(state, action as A)
+			setSavedState(newState)
+			return newState
+		},
+		[reducer, setSavedState],
+	)
 
-  const [_, dispatch] = useReducerWithMiddleware(
-    localStorageReducer,
-    savedState,
-    middlewareFns,
-    afterwareFns,
-  );
+	const [_, dispatch] = useReducerWithMiddleware(
+		localStorageReducer,
+		savedState,
+		middlewareFns,
+		afterwareFns,
+	)
 
-  // resync the reducer state with savedState
-  useEffect(() => {
-    dispatch({ type: "sync", payload: savedState });
-  }, [savedState, dispatch]);
+	// resync the reducer state with savedState
+	useEffect(() => {
+		dispatch({ type: "sync", payload: savedState })
+	}, [savedState, dispatch])
 
-  return [savedState, dispatch] as const;
-};
+	return [savedState, dispatch] as const
+}
 
 export const useReducerWithMiddleware = <S, A>(
-  // TODO: creae a type like Reducer for this
-  reducer: AugmentedReducer<S, A>,
-  initialState: S,
-  middlewareFns: Array<ReducerMiddlewareFn<S, A>>,
-  afterwareFns: Array<ReducerMiddlewareFn<S, A>>,
+	// TODO: creae a type like Reducer for this
+	reducer: AugmentedReducer<S, A>,
+	initialState: S,
+	middlewareFns: Array<ReducerMiddlewareFn<S, A>>,
+	afterwareFns: Array<ReducerMiddlewareFn<S, A>>,
 ) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, initialState)
 
-  const dispatchWithMiddleware = useCallback(
-    (action: A | SyncAction<S>) => {
-      middlewareFns.forEach((middlewareFn) => middlewareFn(action, state));
-      dispatch(action);
-      afterwareFns.forEach((afterwareFn) => afterwareFn(action, state));
-    },
-    [middlewareFns, afterwareFns, state],
-  );
+	const dispatchWithMiddleware = useCallback(
+		(action: A | SyncAction<S>) => {
+			middlewareFns.forEach((middlewareFn) => middlewareFn(action, state))
+			dispatch(action)
+			afterwareFns.forEach((afterwareFn) => afterwareFn(action, state))
+		},
+		[middlewareFns, afterwareFns, state],
+	)
 
-  return [state, dispatchWithMiddleware] as const;
-};
+	return [state, dispatchWithMiddleware] as const
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
-import { renderHook } from "@testing-library/react";
-import { describe, it, expect } from "bun:test";
-import { useLocalStorageReducer } from "../src/";
+import { describe, expect, it } from "bun:test"
+import { renderHook } from "@testing-library/react"
+import { useLocalStorageReducer } from "../src/"
 
 describe("should", () => {
-  it("example", () => {
-    const { result } = renderHook(() =>
-      useLocalStorageReducer("key", (state, action) => state, 0),
-    );
-    expect(result.current[0]).toBe(0);
-  });
-});
+	it("example", () => {
+		const { result } = renderHook(() =>
+			useLocalStorageReducer("key", (state, action) => state, 0),
+		)
+		expect(result.current[0]).toBe(0)
+	})
+})
