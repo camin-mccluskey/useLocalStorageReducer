810f5982c251250512245a60bdea7abf267c4a17
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2024-08-14 06:16:18 +0300

    Tidy up tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,18 +2,62 @@ import { describe, expect, it } from 'bun:test'
 import { renderHook } from '@testing-library/react'
 import { useLocalStorageReducer } from '../src/'
 
-describe('should', () => {
-	it('example', () => {
-		const { result } = renderHook(
-			() =>
-				useLocalStorageReducer(
-					'key',
-					(state: number, action: { type: 'inc' }) => state,
-					0,
-				),
-			{ hydrate: true },
+type TestAction =
+	| { type: 'inc' }
+	| { type: 'dec' }
+	| { type: 'set'; value: number }
+const TEST_LS_KEY = 'useLocalStorageReducer-test-key'
+const TEST_INITIAL_STATE = 0
+const TEST_REDUCER = (state: number, action: TestAction) => {
+	const { type } = action
+	switch (type) {
+		case 'inc':
+			return state + 1
+		case 'dec':
+			return state - 1
+		case 'set':
+			return action.value
+		default:
+			return type satisfies never
+	}
+}
+
+describe('useLocalStorageReducer', () => {
+	it('Initialises with Local Storage value by default', () => {
+		const { result } = renderHook(() =>
+			useLocalStorageReducer(TEST_LS_KEY, TEST_REDUCER, TEST_INITIAL_STATE),
 		)
-		const [state, dispatch] = result.current
+		const [state, _] = result.current
+		expect(state).toBe(0)
+	})
+
+	it('Initialises with Local Storage value when initializeWithValue is true', () => {
+		const { result } = renderHook(() =>
+			useLocalStorageReducer(
+				TEST_LS_KEY,
+				TEST_REDUCER,
+				TEST_INITIAL_STATE,
+				[],
+				[],
+				{ initializeWithValue: true },
+			),
+		)
+		const [state, _] = result.current
+		expect(state).toBe(0)
+	})
+
+	it('Initialises with initial value when initializeWithValue is false', () => {
+		const { result } = renderHook(() =>
+			useLocalStorageReducer(
+				TEST_LS_KEY,
+				TEST_REDUCER,
+				TEST_INITIAL_STATE,
+				[],
+				[],
+				{ initializeWithValue: false },
+			),
+		)
+		const [state, _] = result.current
 		expect(state).toBe(0)
 	})
 })
